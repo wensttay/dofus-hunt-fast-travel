0e61f5debfe9998893a9e6212ccefb8426165284
Wensttay <www.wensttay@gmail.com>
2020-03-08 13:18:40 -0300

    Fix location format

--- fasttravel.js
+++ fasttravel.js
@@ -5,9 +5,10 @@ $('#hintName').addEventListener('change', function() {
     button.style = " font-size: 20px; background-color: #2f2f2f; color: rgb(204, 204, 204); width: 60px; float: right; position: relative; margin-top: -35px; margin-right: 5px; z-index: 100;"
     
     button.addEventListener("click", function() {
-        navigator.clipboard.writeText("/travel [" 
-        + currentSelectedHint.x + "," 
-        + currentSelectedHint.y + "]");
+        navigator.clipboard.writeText("/travel " 
+        + currentSelectedHint.x 
+        + " " 
+        + currentSelectedHint.y);
     });
 
     var child = document.getElementById('cords');
