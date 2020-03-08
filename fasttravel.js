7fd8bba5b4fa8acdf4f697c2a0285aba6ba602cc
Wensttay <www.wensttay@gmail.com>
2020-03-08 13:05:16 -0300

    Creating a Chrome Extencion to use the Script

--- fasttravel.js
+++ fasttravel.js
@@ -1,13 +1,20 @@
-$('#hintName').addEventListener('change', function() {
-    var button = document.createElement("button");
-    button.innerHTML = "GO"; 
-    button.id = "cords";
-    button.style = " font-size: 20px; background-color: #2f2f2f; color: rgb(204, 204, 204); width: 60px; float: right; position: relative; margin-top: -35px; margin-right: 5px; z-index: 100;"
+document.getElementById('hintName').addEventListener('change', function() {
+    var button = document.createElement('button');
+    button.innerHTML = 'GO'; 
+    button.id = 'cords';
+    button.style = ' font-size: 20px; background-color: #2f2f2f; color: rgb(204, 204, 204); width: 60px; float: right; position: relative; margin-top: -35px; margin-right: 10px; z-index: 100;'
     
-    button.addEventListener("click", function() {
-        navigator.clipboard.writeText("/travel [" 
-        + currentSelectedHint.x + "," 
-        + currentSelectedHint.y + "]");
+    // The extension doesn't have access to page variables
+    // [TODO]: Search a better way to do that
+    button.addEventListener('click', function() {
+        var cords = document.getElementById('secondLine')
+            .innerHTML
+            .split("<span>").join("")
+            .split("</span>").join("")
+            .split(" ").join("")
+            .split(";").join(",");
+
+        navigator.clipboard.writeText("/travel " + cords);
     });
 
     var child = document.getElementById('cords');
@@ -18,3 +25,4 @@ $('#hintName').addEventListener('change', function() {
         document.getElementById('result').appendChild(button);
     }
 });
+
