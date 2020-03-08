4a10ad555bca26d782b6b57494c05afd21909039
Wensttay <www.wensttay@gmail.com>
2020-03-08 11:37:49 -0300

    Changing to use page variables

--- fasttravel.js
+++ fasttravel.js
@@ -2,17 +2,12 @@ $('#hintName').addEventListener('change', function() {
     var button = document.createElement("button");
     button.innerHTML = "GO"; 
     button.id = "cords";
-    button.style = " font-size: 24px; background-color: #2f2f2f; color: rgb(204, 204, 204); width: 60px; float: right; position: relative; margin-top: -35px; margin-right: 5px; z-index: 100;"
+    button.style = " font-size: 20px; background-color: #2f2f2f; color: rgb(204, 204, 204); width: 60px; float: right; position: relative; margin-top: -35px; margin-right: 5px; z-index: 100;"
     
     button.addEventListener("click", function() {
-        var cords = document.getElementById('secondLine')
-            .innerHTML
-            .split("<span>").join("")
-            .split("</span>").join("")
-            .split(" ").join("")
-            .split(";").join(",");
-
-        navigator.clipboard.writeText("/travel " + cords);
+        navigator.clipboard.writeText("/travel [" 
+        + currentSelectedHint.x + "," 
+        + currentSelectedHint.y + "]");
     });
 
     var child = document.getElementById('cords');
