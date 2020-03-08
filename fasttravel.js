0698243b2fbbe45dcbc474a71ba6da56e22a53dc
Wensttay <www.wensttay@gmail.com>
2020-03-08 11:11:57 -0300

    Dofus Hunt Fast Travel Script

---
+++ fasttravel.js
@@ -0,0 +1,25 @@
+$('#hintName').addEventListener('change', function() {
+    var button = document.createElement("button");
+    button.innerHTML = "GO"; 
+    button.id = "cords";
+    button.style = " font-size: 24px; background-color: #2f2f2f; color: rgb(204, 204, 204); width: 60px; float: right; position: relative; margin-top: -35px; margin-right: 5px; z-index: 100;"
+    
+    button.addEventListener("click", function() {
+        var cords = document.getElementById('secondLine')
+            .innerHTML
+            .split("<span>").join("")
+            .split("</span>").join("")
+            .split(" ").join("")
+            .split(";").join(",");
+
+        navigator.clipboard.writeText("/travel " + cords);
+    });
+
+    var child = document.getElementById('cords');
+    if (child != undefined) {
+        child.parentNode.removeChild(child);
+        document.getElementById('result').appendChild(button);
+    } else { 
+        document.getElementById('result').appendChild(button);
+    }
+});
