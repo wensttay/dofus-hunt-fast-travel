734ca885c10d6676a574423b1afb823fe2e808c2
Wensttay <www.wensttay@gmail.com>
2020-03-14 01:02:17 -0300

    Adding cookies to the switch value

--- js/fasttravel.js
+++ js/fasttravel.js
@@ -1,3 +1,4 @@
+// SWITCH
 var switchDiv = document.createElement('div');
 switchDiv.classList.add('switch-box');
 
@@ -8,7 +9,8 @@ switchLabel.title = chrome.i18n.getMessage("switch");
 
 var switchInput = document.createElement('input');
 switchInput.type = 'checkbox';
-switchInput.checked = false;
+var switched = getCookie("switchChecked");
+switchInput.checked = switched != null ? switched : false;
 
 var switchSpan = document.createElement('span');
 switchSpan.classList.add('slider');
@@ -18,7 +20,13 @@ switchLabel.appendChild(switchInput);
 switchLabel.appendChild(switchSpan);
 switchDiv.appendChild(switchLabel);
 
+
+
+
+
+// COPY POS EVENTS
 switchInput.addEventListener('change', function() {
+    setCookie("switchChecked", switchInput.checked, 30);
     copyToClipboard();
 })
 
@@ -55,4 +63,34 @@ function copyToClipboard() {
     } else {
         navigator.clipboard.writeText(cords);
     }
+};
+
+
+
+
+
+// COOKIES
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+};
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+};
+
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
 };
